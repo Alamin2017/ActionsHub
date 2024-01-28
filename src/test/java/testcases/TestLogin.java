846e030f25469ced4127a69dc33f200dfd79cbfb
Al-Amin <alamincse12@gmail.com>
2024-01-28 22:01:54 +0600

    new test is added

--- src/test/java/testcases/TestLogin.java
+++ src/test/java/testcases/TestLogin.java
@@ -13,4 +13,11 @@ public class TestLogin extends BaseEnv {
         LoginLocators.enterEmail(LoginData.userEmail);
         Thread.sleep(2000);
     }
+    @Test(priority = 1,description = "Login test",retryAnalyzer = Retry.class)
+    public void LoginTest2() throws InterruptedException {
+        driver.get("http://teststore.automationtesting.co.uk/");
+        LoginLocators.clickSignInButton();
+        LoginLocators.enterEmail(LoginData.userEmail);
+        Thread.sleep(2000);
+    }
 }
