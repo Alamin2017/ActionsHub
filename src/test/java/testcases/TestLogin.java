8f7b7dafd3e645dc634ee3b72d9331552ab0b67f
Al-Amin <alamincse12@gmail.com>
2024-01-30 22:21:46 +0600

    new updated

--- src/test/java/testcases/TestLogin.java
+++ src/test/java/testcases/TestLogin.java
@@ -6,14 +6,14 @@ import pageobject.LoginLocators;
 import testdata.LoginData;
 
 public class TestLogin extends BaseEnv {
-    @Test(priority = 1,description = "Login test",retryAnalyzer = Retry.class)
+    @Test(retryAnalyzer = Retry.class)
     public void LoginTest() throws InterruptedException {
         driver.get("http://teststore.automationtesting.co.uk/");
         LoginLocators.clickSignInButton();
         LoginLocators.enterEmail(LoginData.userEmail);
         Thread.sleep(2000);
     }
-    @Test(priority = 1,description = "Login test",retryAnalyzer = Retry.class)
+    @Test(retryAnalyzer = Retry.class)
     public void LoginTest2() throws InterruptedException {
         driver.get("http://teststore.automationtesting.co.uk/");
         LoginLocators.clickSignInButton();
